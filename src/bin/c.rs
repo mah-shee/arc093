fabd5027b8e3d5cbbeac0fbefca81062f14ca37c
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-21 22:15:56 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,22 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        mut a: [isize; n]
+    }
+    let mut b: Vec<isize> = vec![0];
+    b.append(&mut a);
+    b.push(0);
+
+    let mut total = 0;
+    for i in 0..n + 1 {
+        total += (b[i + 1] - b[i]).abs();
+    }
+    for i in 1..n + 1 {
+        println!(
+            "{}",
+            total + (b[i - 1] - b[i + 1]).abs()
+                - ((b[i - 1] - b[i]).abs() + (b[i] - b[i + 1]).abs())
+        );
     }
 }
